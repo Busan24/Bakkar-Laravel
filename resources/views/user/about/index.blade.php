1500db3b54ef9fc6ca1a5563644768d6dae6a244
Busan24 <budysantoso1120@gmail.com>
2024-11-10 17:44:07 +0700

    tampilan

--- resources/views/user/about/index.blade.php
+++ resources/views/user/about/index.blade.php
@@ -4,12 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tentang Kami</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="{{ asset('assets/img/logo_bakkar.png') }}" />
-    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <script src="https://cdn.tailwindcss.com"></script>
-    
     <style>
         
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap');
