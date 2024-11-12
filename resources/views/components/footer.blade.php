acf5acd5c0bdc8acd4307226e24c2fedaebe20fd
Busan24 <budysantoso1120@gmail.com>
2024-11-12 09:26:41 +0700

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,67 +1,40 @@
-<footer class="relative bg-black pt-8 pb-6">
-  <div class="container mx-auto px-4">
-    <div class="flex flex-wrap text-left lg:text-left">
-      <div class="w-full lg:w-6/12 px-4">
-        <h4 class="text-3xl text-white">BAKKAR</h4>
-        <h5 class="text-lg mt-0 mb-2 text-white">
-          Find us on any of these platforms, we respond 1-2 business days.
-        </h5>
-        <div class="mt-6 lg:mb-0 mb-6">
-          <button class="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-            <i class="fab fa-twitter"></i></button><button class="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-            <i class="fab fa-facebook-square"></i></button><button class="bg-white text-pink-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-            <i class="fab fa-dribbble"></i></button><button class="bg-white text-blueGray-800 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-            <i class="fab fa-github"></i>
-          </button>
-        </div>
-      </div>
-      <div class="w-full lg:w-6/12 px-4">
-        <div class="flex flex-wrap items-top mb-6">
-          <div class="w-full lg:w-4/12 px-4 ml-auto">
-            <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Useful Links</span>
-            <ul class="list-unstyled">
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/presentation?ref=njs-profile">About Us</a>
-              </li>
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://blog.creative-tim.com?ref=njs-profile">Blog</a>
-              </li>
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.github.com/creativetimofficial?ref=njs-profile">Github</a>
-              </li>
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile">Free Products</a>
-              </li>
-            </ul>
-          </div>
-          <div class="w-full lg:w-4/12 px-4">
-            <span class="block uppercase text-blueGray-500 text-sm font-semibold mb-2">Other Resources</span>
-            <ul class="list-unstyled">
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile">MIT License</a>
-              </li>
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/terms?ref=njs-profile">Terms &amp; Conditions</a>
-              </li>
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/privacy?ref=njs-profile">Privacy Policy</a>
-              </li>
-              <li>
-                <a class="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm" href="https://creative-tim.com/contact-us?ref=njs-profile">Contact Us</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+<footer class="bg-gray-700 text-white py-8">
+  <div class="container mx-auto flex flex-wrap justify-between items-start px-4">
+    <!-- Logo -->
+    <div class="mb-6">
+      <h2 class="text-3xl font-bold font-sans">Bakkar Fried Chicken</h2>
     </div>
-    <hr class="my-6 border-blueGray-300">
-    <div class="flex flex-wrap items-center md:justify-between justify-center">
-      <div class="w-full md:w-4/12 px-4 mx-auto text-center">
-        <div class="text-sm text-blueGray-500 font-semibold py-1">
-          Copyright © <span id="get-current-year">2021</span><a href="https://www.creative-tim.com/product/notus-js" class="text-blueGray-500 hover:text-gray-800" target="_blank"> Notus JS by
-          <a href="https://www.creative-tim.com?ref=njs-profile" class="text-blueGray-500 hover:text-blueGray-800">Creative Tim</a>.
-        </div>
-      </div>
+
+    <!-- Links -->
+    <div class="mb-6">
+      <h3 class="text-lg font-semibold mb-2">Navigasi</h3>
+      <ul class="space-y-2">
+            <li><a href="{{ url('/') }}" class="hover:text-gray-900">Beranda</a></li>
+            <li><a href="{{ url('/menu') }}" class="hover:text-gray-900">Menu</a></li>
+            <li><a href="{{ url('/about') }}" class="hover:text-gray-900">Tentang Kami</a></li>
+      </ul>
     </div>
+
+    <!-- Contact Info -->
+    <div class="mb-6">
+      <h3 class="text-lg font-semibold mb-2">Kontak</h3>
+      <p>Alamat: Jl. Raya No.123, Jakarta</p>
+      <p>Email: info@bakkar.com</p>
+      <p>Telepon: +62 812 3456 7890</p>
+    </div>
+
+    <!-- Social Media -->
+    <div class="flex gap-1">
+      <a href="https://www.instagram.com/bakkar.friedchicken?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" 
+        class="flex items-center text-white hover:text-gray-400">
+        <i class="fab fa-instagram mr-2"></i>
+        <span>bakkar.friedchicken</span>
+      </a>
+    </div>
+  </div>
+
+  <!-- Footer Bottom -->
+  <div class="border-t border-gray-700 mt-8 pt-4 text-center">
+    <p>&copy; 2024 Bakkar. All Rights Reserved.</p>
   </div>
 </footer>
