8a07649caf7e38f1241d3af95691a82edfd1bcc3
Busan24 <budysantoso1120@gmail.com>
2024-11-13 07:18:53 +0700

    update baru

--- resources/views/user/beranda/index.blade.php
+++ resources/views/user/beranda/index.blade.php
@@ -197,13 +197,13 @@
     <section id="konten" class="py-12 pb-32 bg-white">
       <div class="container mx-auto px-6 md:px-12">
           <h2 class="text-3xl font-bold text-gray-800 text-center mb-24 observer-item fade-in-up">Tersedia juga di</h2>
-          <div class="mt-16 flex flex-col sm:flex-row flex-wrap justify-center gap-4">
+          <div class="mt-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
               @foreach($kontens as $konten)
                   <!-- Konten Card -->
-                  <a href="{{ $konten->isi_konten }}" class="group bg-white shadow-md rounded-lg overflow-hidden transform transition hover:scale-105 hover:shadow-xl observer-item fade-in-up w-full sm:w-auto">
-                      <div class="bg-white rounded-lg flex items-center">
-                          <div class="w-full p-4 flex mx-2 justify-center sm:justify-start">
-                              <h3 class="text-xl font-bold text-gray-800 ms-2 text-center sm:text-left">{{ $konten->judul_konten }}</h3>
+                  <a href="{{ $konten->isi_konten }}" class="group bg-white shadow-md rounded-lg overflow-hidden transform transition hover:scale-105 hover:shadow-xl observer-item fade-in-up w-full h-full">
+                      <div class="bg-white rounded-lg flex items-center justify-center h-full">
+                          <div class="w-full p-4 flex justify-center sm:justify-start">
+                              <h3 class="text-xl font-bold text-gray-800 text-center sm:text-left">{{ $konten->judul_konten }}</h3>
                           </div>
                       </div>
                   </a>
